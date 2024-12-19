5ed2227fb1d6a61c4fb693f630a28210653f6970
vicheaaaa <sengvichea11th@gmail.com>
2024-12-19 10:35:44 +0700

    change cureency

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from 'stripe'
 import razorpay from 'razorpay'
 
 // global variables
-const currency = 'inr'
+const currency = 'usd'
 const deliveryCharge = 10
 
 // gateway initialize
